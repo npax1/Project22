24e87a1ff436052ad5f8586d778cb8535a68cb7d
npax1 <prakh55555@gmail.com>
2023-02-28 19:39:24 +0200

    add

--- Source.cpp
+++ Source.cpp
@@ -1,26 +1,25 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
-struct Rectangle {
-    int x, y, width, height;
-    void move(int dx, int dy) {
-        x += dx;
-        y += dy;
-    }
-    void resize(int dw, int dh) {
-        width += dw;
-        height += dh;
-    }
-    void draw() {
-        cout << "Rectangle(" << x << ", " << y << ", " << width << ", " << height << ")" << endl;
-    }
+
+struct Point {
+    int x, y;
 };
 
+double distance(Point p1, Point p2) {
+    int dx = p1.x - p2.x;
+    int dy = p1.y - p2.y;
+    return sqrt(dx * dx + dy * dy);
+}
+
 int main() {
-    Rectangle rect = { 10, 20, 30, 40 };
-    rect.move(5, 5);
-    rect.resize(10, 10);
-    rect.draw();
+
+    Point p1 = { 2, 3 };
+    Point p2 = { 5, 7 };
+
+    double dist = distance(p1, p2);
+    cout << "Distance between (" << p1.x << ", " << p1.y << ") and (" << p2.x << ", " << p2.y << ") is " << dist << endl;
 
     return 0;
 }
